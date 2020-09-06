aafba592ccd1a9b6227a230939831eb20ebaefc7
Leonard Movchan <lanakrit@gmail.com>
2020-09-06 16:12:26 +0300

    inital commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using StockLibrary;
+
+namespace HomeTask_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Product product = new Product("wire", 22.44m, "2020,08,30");
+            Console.WriteLine($"Name: {product.Name}\nPrice: {product.Price}\nDelivery Date: {product.DeliveryDate}");
+
+            Console.WriteLine();
+
+            Product product2 = new Product("sand", 33.55m, "2020,08,23");
+            Console.WriteLine($"Name: {product2.Name}\nPrice: {product2.Price}\nDelivery Date: {product2.DeliveryDate}");
+
+            Console.WriteLine();
+
+            Product product3 = new Product("iron", 120, "2020,08,25");
+            Console.WriteLine($"Name: {product3.Name}\nPrice: {product3.Price}\nDelivery Date: {product3.DeliveryDate}");
+          
+            Stock.Add(product);
+            Stock.Add(product2);
+            Stock.Add(product3);
+            
+            Console.WriteLine();
+
+            string expirationDate = product.ExpirationDateEmulator("2020,08,29");
+            Console.WriteLine($"The expiration date of the {product.Name} is {expirationDate}");
+
+            expirationDate = product2.ExpirationDateEmulator("2020,01,29");
+            Console.WriteLine($"The expiration date of the {product2.Name} is {expirationDate}");
+
+            expirationDate = product3.ExpirationDateEmulator("2019/02/22");
+            Console.WriteLine($"The expiration date of the {product3.Name} is {expirationDate}");
+
+            foreach (var item in Stock.GetProdcuts())
+            {
+                Console.WriteLine($"\n{item}");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ StockLibrary/Product.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StockLibrary
+{
+    public class Stock
+    {
+        private static int volume = 5;
+
+        private static List<Product> stock = new List<Product>(new Product[volume]);
+
+        public static void Add(Product product)
+        {
+            stock.Add(product);
+        }
+
+        public static Product[] GetProdcuts()
+        {
+            return stock.ToArray();
+        }
+    }
+    public class Product
+    {
+        public Product(string name, decimal price, string deliveryDate)
+        {
+            this.Name = name;
+            this.Price = price;
+            this.deliveryDate = deliveryDate;
+            this.ExpiryDate = ExpiryDate;
+        }
+
+        private string name;
+
+        private decimal price;
+            
+        private string deliveryDate;
+
+        private DateTime expirydate;
+
+        public string Name { get { return name; } set { name = value; } }
+        public decimal Price { get { return price; } set { price = value; } }
+        public string DeliveryDate { get { return deliveryDate; } set { deliveryDate = value; } }
+    
+        public DateTime ExpiryDate
+        {
+            get
+            {
+                if (name == "sand")
+                {
+
+                    return expirydate = DateTime.Parse(deliveryDate).AddDays(5);
+                }
+                if (name == "wire")
+                {
+
+                    return expirydate = DateTime.Parse(deliveryDate).AddDays(3);
+                }
+
+                if (name == "iron")
+                {
+                    return expirydate = DateTime.Parse(deliveryDate).AddDays(10);
+                }
+
+
+                return expirydate;
+            }
+            set
+            {
+                if (DateTime.Now > ExpiryDate)
+                {
+                    Console.WriteLine($"Product has expired");
+                }
+
+                else
+                {
+                    expirydate = value;
+                }
+
+            }
+
+        }
+        public override string ToString()
+        {
+            return $"Name:{this.Name}\nPrice: {this.Price}\nDelivery Date: {this.deliveryDate}\nExpiration Date {ExpiryDate}";
+        }
+
+        public string ExpirationDateEmulator(string date)
+        {
+            string expiration = "";
+
+            if (Name == "sand")
+            {
+                expiration = (DateTime.Parse(date).AddDays(5)).ToString();
+            }
+            else if (Name == "wire")
+            {
+                expiration = (DateTime.Parse(date).AddDays(3)).ToString();
+            }
+            else if (Name == "iron")
+            {
+                expiration = (DateTime.Parse(date).AddDays(10)).ToString();
+            }
+
+            return expiration;
+        }
+
+    }
+
+
+}
